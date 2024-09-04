1398da9b495c59ec6bb63deb0c28dce27f92a646
zane <czl970721@gmail.com>
2024-09-03 23:29:33 -0400

    background finish

--- src/Nostalgia/Galaxy.tsx
+++ src/Nostalgia/Galaxy.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Outlines } from '@react-three/drei'
-import { Color, Object3D, OrthographicCamera, Vector3 } from 'three';
-import { degToRad, randInt } from 'three/src/math/MathUtils.js';
+import { Outlines, Point, PointMaterial, Points } from '@react-three/drei'
+import { BufferGeometry, Color, Euler, Group, OrthographicCamera, Path, Vector3 } from 'three';
+import { degToRad, MathUtils, randInt } from 'three/src/math/MathUtils.js';
+import { Bloom, EffectComposer, Selection, Select } from '@react-three/postprocessing';
 
 const SunSize = 1
 const d = 20;
@@ -11,8 +12,8 @@ const WHITE = 0xffffff;
 const BLACK = 0x000000;
 
 function Galaxy() {
-    const orthCamera = new OrthographicCamera(- d, d, d, d, 1, 50)
-    orthCamera.position.set(0, 0, 10);
+    const orthCamera = new OrthographicCamera(- d, d, d, d, 1, 200)
+    orthCamera.position.set(0, 0, 30);
     orthCamera.zoom = 50
 
     return (
@@ -21,11 +22,14 @@ function Galaxy() {
             camera={orthCamera}
         >
             <Sun />
+            <Background />
+
             <Planet radius={0.1} distance={0.5} speed={15} />
             <Planet radius={0.15} distance={1.25} speed={8} />
             <Planet radius={0.2} distance={3} speed={4} />
-            <Planet radius={0.4} distance={7} speed={2} incline={0.4} />
-            <Planet radius={0.5} distance={17} speed={2} />
+            <Planet radius={0.4} distance={7} speed={2} />
+            <Planet radius={0.5} distance={12} speed={1.3} incline={0.3} />
+            <Planet radius={0.3} distance={18} speed={0.5} />
         </Canvas >
     )
 }
@@ -34,11 +38,19 @@ function Sun() {
     return (
         <>
             <pointLight position={[0, 0, 0]} intensity={0.2} decay={0.2} />
-            <mesh position={[0, 0, 0]}>
-                <sphereGeometry args={[SunSize]} />
-                <meshStandardMaterial color={WHITE} emissive={WHITE} emissiveIntensity={10} />
-                <Outlines screenspace thickness={outlinesThickness} color={BLACK} />
-            </mesh>
+            <Selection>
+                <EffectComposer autoClear={false}>
+                    <Bloom luminanceThreshold={0.9} luminanceSmoothing={1} intensity={.1} />
+                </EffectComposer>
+                <Select enabled>
+                    <mesh position={[0, 0, 0]}>
+                        <sphereGeometry args={[SunSize]} />
+                        <meshStandardMaterial toneMapped={false}
+                            color={WHITE} emissive={WHITE} emissiveIntensity={10} />
+                        <Outlines screenspace thickness={outlinesThickness} color={BLACK} />
+                    </mesh>
+                </Select>
+            </Selection>
         </>
     );
 }
@@ -52,27 +64,70 @@ interface PlanetProps {
 
 
 function Planet({ radius, distance, speed, incline = 0.2 }: PlanetProps) {
-    const objRef = React.useRef<Object3D>(null);
-    const axis = new Vector3(0, 1, 0);
+    const pivotRef = React.useRef<Group | null>(null);
 
     React.useEffect(() => {
-        objRef.current?.rotateZ(-incline);
-        objRef.current?.rotateOnAxis(axis, degToRad(degToRad(speed) * randInt(20, 1000)));
-    }, [objRef])
+        pivotRef.current?.rotateOnWorldAxis(new Vector3(-1, 0, 1), -incline);
+        pivotRef.current?.rotateY(degToRad(speed) * randInt(10, 1000));
+        // console.log(pivotRef.current?.getWorldDirection(new Vector3(0, 0, 0)));
+    }, [pivotRef])
+
 
     useFrame(() => {
-        objRef.current?.rotateOnAxis(axis, degToRad(degToRad(speed)))
+        pivotRef.current?.rotateY(degToRad(speed / 30))
     })
 
     return (
-        <object3D ref={objRef}>
-            <mesh castShadow receiveShadow position={[distance + SunSize, 0, 0]}>
-                <sphereGeometry args={[radius]} />
-                <meshStandardMaterial color={WHITE} />
-                <Outlines screenspace thickness={outlinesThickness} color={WHITE} />
-            </mesh>
-        </object3D>
+        <>
+            <group ref={pivotRef} position={[0, 0, 0]}>
+                <mesh castShadow receiveShadow position={[distance + SunSize, 0, 0]}>
+                    <sphereGeometry args={[radius]} />
+                    <meshStandardMaterial color={WHITE} />
+                    <Outlines screenspace thickness={outlinesThickness} color={WHITE} />
+                </mesh>
+                <lineLoop geometry={CircleLineGeometry(distance + SunSize)} rotation={new Euler(Math.PI / 2, 0, 0)}>
+                    <lineBasicMaterial color={0x444444} linewidth={1} />
+                </lineLoop>
+            </group>
+        </>
     )
+}
+
+function Background() {
+    return (
+        <>
+            <BackgroundLayer color={WHITE} />
+            <BackgroundLayer color={0x666666} />
+            <BackgroundLayer color={0x222222} />
+        </>
+    )
+}
+
+function BackgroundLayer({ color }: { color: unknown }) {
+    const count = 750;
+
+    const positions = React.useMemo(() => Array.from({ length: count }, (i) => [
+        MathUtils.randFloatSpread(40),
+        MathUtils.randFloatSpread(20),
+        MathUtils.randFloatSpread(1) - 35,
+    ]), []);
+
+    return (
+        <Points limit={count}>
+            <PointMaterial vertexColors size={.2}
+                color={color} emissive={color}
+            />
+            {positions.map((position, i) => (
+                <Point key={i} position={new Vector3(...position)} />
+            ))}
+        </Points>
+    )
+}
+
+function CircleLineGeometry(radius: number) {
+    return new BufferGeometry().setFromPoints(
+        new Path().absarc(0, 0, radius, 0, Math.PI * 2).getSpacedPoints(radius * radius * 50)
+    );
 }
 
 export default Galaxy;
