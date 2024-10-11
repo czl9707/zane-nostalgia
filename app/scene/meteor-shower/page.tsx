1b223f3baac9d409c99752dec7081634c44aedc8
zane <czl970721@gmail.com>
2024-10-10 20:06:30 -0400

    good with svg

--- app/scene/meteor-shower/page.tsx
+++ app/scene/meteor-shower/page.tsx
@@ -8,7 +8,6 @@ interface MeteorShowerProps {
     dense?: number,
     height?: number,
     width?: number,
-    speed?: number,
 }
 
 function MeteorShower({
@@ -18,12 +17,19 @@ function MeteorShower({
     dense = 1,
     height = DEFAULT_SVG_HEIGHT,
     width = DEFAULT_SVG_WIDTH,
-    speed = 2,
 }: MeteorShowerProps) {
-    const real_width = 500 / height * width;
-    const amount_per_px = dense * 0.03;
+    const realWidth = Math.floor(500 / height * width);
+    const meteorInitRange = realWidth + Math.floor(500 * Math.tan((rotateDegree * Math.PI) / 180));
+    const meteorInitRangeStart = rotateDegree > 0 ? 0 : - Math.floor(500 * Math.tan((rotateDegree * Math.PI) / 180));
 
-    return (<svg viewBox={`0 0 ${real_width} 500`} height={`${height}px`} width={`${width}px`}>
+    const meteorCount = Math.floor(meteorInitRange * dense * 0.1);
+
+    const meteorAnimationTimeRange = 60;
+
+    const METEORSIZEVARIENTS = 3;
+    const METEORINITVARIANTS = Math.floor(meteorAnimationTimeRange / 3);
+
+    return (<svg viewBox={`0 0 ${realWidth} 500`} height={`${height}px`} width={`${width}px`}>
         <defs>
             <linearGradient id="tailGradient" gradientTransform="rotate(0)">
                 <stop offset="0%" stopColor={color} />
@@ -39,34 +45,36 @@ function MeteorShower({
                 <rect y={-0.5} width={300} height={1} opacity={0.6} fill="url('#tailGradient')" />
             </g>
 
-            <g id="meteor1">
-                <use href="#meteorGeo" transform={`rotate(-${rotateDegree})`}>
-                    <animate attributeName="x" values="0;-1500" dur={`${100 / speed}s`} repeatCount="indefinite" />
-                </use>
-            </g>
-            <g id="meteor2">
-                <use href="#meteorGeo" transform={`rotate(-${rotateDegree})`}>
-                    <animate attributeName="x" values="0;-1500" dur={`${100 / speed}s`} begin="3s" repeatCount="indefinite" />
-                </use>
-            </g>
-            <g id="meteor3">
-                <use href="#meteorGeo" transform={`rotate(-${rotateDegree})`}>
-                    <animate attributeName="x" values="0;-1500" dur={`${100 / speed}s`} begin="6s" repeatCount="indefinite" />
-                </use>
-            </g>
+            {
+                [...Array(METEORSIZEVARIENTS)].map(((_, i) => (
+                    <React.Fragment key={i}>
+                        {
+                            [...Array(METEORINITVARIANTS)].map(((_, j) => (
+                                <g id={`meteor${i}${j}`} key={j}>
+                                    <use href="#meteorGeo" transform={`rotate(-${rotateDegree})`} opacity={0.5 + i * 0.2}>
+                                        <animate attributeName="x" values="0;-1500" dur={`${meteorAnimationTimeRange * (1 - i * 0.25)}s`}
+                                            begin={`${j * 3}s`} repeatCount="indefinite" />
+                                    </use>
+                                </g>
+                            )))
+                        }
+                    </React.Fragment>
+                )))
+            }
         </defs>
 
-        <rect width={`${real_width}px`} height="500px" fill={backgroundColor} />
+        <rect width={`${realWidth}px`} height="500px" fill={backgroundColor} />
         {
-            [...Array(Math.floor(amount_per_px * real_width))].map((_, i) => (
-                <React.Fragment key={"x" + i}>
-                    <use x={Math.random() * (real_width + 500)} y={Math.random() * -50} href="#meteor1" />
-                    <use x={Math.random() * (real_width + 500)} y={Math.random() * -50} href="#meteor2" />
-                    <use x={Math.random() * (real_width + 500)} y={Math.random() * -50} href="#meteor3" />
-                </React.Fragment>
+            [...Array(meteorCount)].map((_, i) => (
+                <use x={randomInt(meteorInitRange, meteorInitRangeStart)} y={randomInt(50, -55)}
+                    href={`#meteor${randomInt(METEORSIZEVARIENTS)}${randomInt(METEORINITVARIANTS)}`} key={i} />
             ))
         }
     </svg>)
+}
+
+function randomInt(range: number, start: number = 0): number {
+    return Math.floor(Math.random() * range + start);
 }
 
 export default function Page() {
