1924283fe4702567d448544e04ac32cdfda135e1
zane <czl970721@gmail.com>
2024-10-22 08:57:06 -0400

    404 !

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { redirect } from "next/navigation"
+
 export default function NotFound() {
-    return null;
+    redirect("/scenes/404")
 }
---
+++ app/scene-components/utils/math-utils.ts
@@ -0,0 +1,10 @@
+function randomMatrix(itemCount: number, itemLength: number): Array<Array<number>> {
+    return [...Array(itemCount)].map((_) => [...Array(itemLength)].map((__) => Math.random()))
+}
+
+function randomFitToInt(num: number, range: number, start: number = 0): number {
+    return Math.floor(num * range + start);
+}
+
+
+export { randomMatrix, randomFitToInt };
