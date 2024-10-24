8c143227ebf8cb4bf41227cdedc0bcfab753d50b
zane <czl970721@gmail.com>
2024-10-23 23:30:15 -0400

    fix lint

--- app/ui-components/layout/header-bar.tsx
+++ app/ui-components/layout/header-bar.tsx
@@ -1,12 +1,12 @@
 import { styled } from "@pigment-css/react";
 
 
-const HeaderBar = styled("div")(({ theme }) => ({
+const HeaderBar = styled("div")({
     padding: "0 4rem", zIndex: 1000,
     height: "4rem", top: "0", left: 0, right: 0,
     position: "fixed", backgroundColor: "transparent",
     display: "flex", flexDirection: "row", alignItems: "center",
     gap: "1rem", justifyContent: "flex-end",
-}));
+});
 
 export default HeaderBar;
