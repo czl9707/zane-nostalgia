f2d5fce7aa3c4edbac8a15d75e97a7ee547f35cc
zane <czl970721@gmail.com>
2024-11-03 20:19:10 -0500

    move into suspense

--- app/scenes/[scene]/layout.tsx
+++ app/scenes/[scene]/layout.tsx
@@ -1,35 +1,17 @@
 "use client"
 
 import * as React from 'react'
-import { useSearchParams } from 'next/navigation';
-import { styled } from '@pigment-css/react';
-
-import { defaultSizeParameterResolver } from '@/app/scene-components/utils/resolver'
 import Panel from '@/app/components/ui/panel';
-
-
-const SVGContainer = styled('svg')(({ theme }) => ({
-    position: "fixed", left: "50%", top: "50%",
-    transform: "translate(-50%, -50%)",
-    transition: `all ${theme.transition.short} linear`,
-    boxSizing: "content-box",
-}))
 
 export default function SceneLayout({ children }: {
     children: React.ReactNode,
 }) {
-    const queryParam = useSearchParams();
-    const sizeParam = defaultSizeParameterResolver({
-        width: queryParam.get("width") ?? undefined,
-        height: queryParam.get("height") ?? undefined
-    });
-
     return (
         <>
             <Panel style={{ position: "fixed", inset: 0 }} />
-            <SVGContainer viewBox={`0 0 ${sizeParam.width} ${sizeParam.height}`} height="96%" width="96%" preserveAspectRatio="xMidYMid meet">
+            <React.Suspense >
                 {children}
-            </SVGContainer>
+            </React.Suspense>
         </>
     )
 }
--- app/scenes/[scene]/page.tsx
+++ app/scenes/[scene]/page.tsx
@@ -2,12 +2,20 @@ import { defaultSceneSizeMetaData } from "@/app/scene-components/utils/constants
 import { fetchScene, fetchSceneMetas } from "@/app/scene-components/utils/fetch-scenes";
 import { defaultParameterResolver, defaultSizeParameterResolver, resolveParameterConstraints } from "@/app/scene-components/utils/resolver";
 import { SceneModule } from "@/app/scene-components/utils/types";
+import { styled } from "@pigment-css/react";
 
 
 export async function generateStaticParams(): Promise<{ scene: string }[]> {
     const scenes = await fetchSceneMetas();
     return scenes.map(s => ({ scene: s.route }));
 }
+
+const SVGContainer = styled('svg')(({ theme }) => ({
+    position: "fixed", left: "50%", top: "50%",
+    transform: "translate(-50%, -50%)",
+    transition: `all ${theme.transition.short} linear`,
+    boxSizing: "content-box",
+}))
 
 export default async function Page({ params, searchParams }: { params: { scene: string }, searchParams: { [key: string]: string } }) {
     const sceneModule: SceneModule = await fetchScene(params.scene);
@@ -18,7 +26,9 @@ export default async function Page({ params, searchParams }: { params: { scene: 
     const resolved = resolveParameterConstraints({ ...resolvedParam, ...resolvedSizeParam }, { ...sceneModule.meta, ...defaultSceneSizeMetaData });
 
     return (
-        <SceneComponent {...resolved} />
+        <SVGContainer viewBox={`0 0 ${resolvedSizeParam.width} ${resolvedSizeParam.height}`} height="96%" width="96%" preserveAspectRatio="xMidYMid meet">
+            <SceneComponent {...resolved} />
+        </SVGContainer>
     )
 }
 
