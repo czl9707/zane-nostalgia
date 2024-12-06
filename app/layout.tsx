c031c3b497c28cbf22826231b86fdeb02c0b4f4e
zane <czl970721@gmail.com>
2024-12-05 22:13:03 -0500

    robots.txt typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:site_name" content="zane-nostalgia.kiyo-n-zane.com" />
         <meta property="og:title" content="Zane's Nostalgia" />
