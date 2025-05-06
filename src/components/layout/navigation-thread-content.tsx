b338efb7eb86aa295e6da9b9a3d0e750bfd4c7ba
zane <czl970721@gmail.com>
2025-05-05 21:27:52 -0400

    fix history

--- src/components/layout/navigation-thread-content.tsx
+++ src/components/layout/navigation-thread-content.tsx
@@ -31,7 +31,7 @@ export default async function NavigationThreadContent() {
             </Link>
             <Divider />
             <AccordionGroup type="multiple" defaultValue={["Scenes"]}>
-                <Accordion name={"Scenes"} value={"Scenes"}>
+                <Accordion name={<T.Body>Scenes</T.Body>} value={"Scenes"}>
                     {
                         sceneMetas.map(({ Icon, name, route }) => (
                             <Link href={`/scenes/${route}`} key={name}>
